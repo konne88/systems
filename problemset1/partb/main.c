26d46874cf52f96dd6eb021c2364397239b491ac
Konstantin Weitz <weitzkon@uw.edu>
2016-10-05 15:34:28 -0700

    original example

--- problemset1/partb/main.c
+++ problemset1/partb/main.c
@@ -1,263 +1,295 @@
-/*
-; thread pool (pthreads)
-
-; worker takes file name, read file, returns a pointer to the file
-
-main 
-
-; args : ip, port
-
-
-
-; open tcp connection
-; read file path, load file, return over tcp
-; close tcp connection
-*/
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/ioctl.h>
-#include <sys/poll.h>
+/* Inspired by:
+ * https://github.com/jasonish/libevent-examples/blob/master/echo-server/libevent_echosrv2.c
+ */
+#include <sys/types.h>
 #include <sys/socket.h>
-#include <sys/time.h>
 #include <netinet/in.h>
+
+/* For inet_ntoa. */
+#include <arpa/inet.h>
+
+/* Required by event.h. */
+#include <sys/time.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
 #include <errno.h>
+#include <err.h>
 
-#define TRUE             1
-#define FALSE            0
+/* Easy sensible linked lists. */
+#include "queue.h"
 
-void error(char* msg) {
-  perror(msg);
-  exit(-1);
-}
+/* Libevent. */
+#include <event.h>
 
-int main (int argc, char *argv[])
+/* Port to listen on. */
+#define SERVER_PORT 5555
+
+/* Length of each buffer in the buffer queue.  Also becomes the amount
+ * of data we try to read per call to read(2). */
+#define BUFLEN 1024
+
+/**
+ * In event based programming we need to queue up data to be written
+ * until we are told by libevent that we can write.  This is a simple
+ * queue of buffers to be written implemented by a TAILQ from queue.h.
+ */
+struct bufferq {
+	/* The buffer. */
+	u_char *buf;
+
+	/* The length of buf. */
+	int len;
+
+	/* The offset into buf to start writing from. */
+	int offset;
+	
+	/* For the linked list structure. */
+	TAILQ_ENTRY(bufferq) entries;
+};
+
+/**
+ * A struct for client specific data, also includes pointer to create
+ * a list of clients.
+ *
+ * In event based programming it is usually necessary to keep some
+ * sort of object per client for state information.
+ */
+struct client {
+	/* Events. We need 2 event structures, one for read event
+	 * notification and the other for writing. */
+	struct event ev_read;
+	struct event ev_write;
+
+	/* This is the queue of data to be written to this client. As
+	 * we can't call write(2) until libevent tells us the socket
+	 * is ready for writing. */
+	TAILQ_HEAD(, bufferq) writeq;
+};
+
+/**
+ * Set a socket to non-blocking mode.
+ */
+int
+setnonblock(int fd)
 {
+	int flags;
 
-  int port = 12345
-  int listen_sd = socket(AF_INET, SOCK_STREAM, 0);
-  if (listen_sd < 0)
-    error("socket() failed");
+	flags = fcntl(fd, F_GETFL);
+	if (flags < 0)
+		return flags;
+	flags |= O_NONBLOCK;
+	if (fcntl(fd, F_SETFL, flags) < 0)
+		return -1;
 
-  /*************************************************************/
-  /* Allow socket descriptor to be reuseable                   */
-  /*************************************************************/
-  /*
-  rc = setsockopt(listen_sd, SOL_SOCKET,  SO_REUSEADDR,
-                  (char *)&on, sizeof(on));
-  if (rc < 0)
-  {
-    perror("setsockopt() failed");
-    close(listen_sd);
-    exit(-1);
-  }
-  */
-
-  /*************************************************************/
-  /* Set socket to be nonblocking. All of the sockets for    */
-  /* the incoming connections will also be nonblocking since  */
-  /* they will inherit that state from the listening socket.   */
-  /*************************************************************/
-  if (ioctl(listen_sd, FIONBIO, (char *)&on) < 0) {
-    close(listen_sd);
-    error("ioctl() failed");
-  }
-
-  struct sockaddr_in addr;
-  memset(&addr, 0, sizeof(addr));
-  addr.sin_family      = AF_INET;
-  addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  addr.sin_port        = htons(port);
-  if (bind(listen_sd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-    close(listen_sd);
-    error("bind() failed");
-  }
-
-  if (listen(listen_sd, 32) < 0) {
-    close(listen_sd);
-    error("listen() failed");
-  }
-
-  struct pollfd fds[200];
-  memset(fds, 0 , sizeof(fds));
-
-  int nfds = 1;
-  fds[0].fd = listen_sd;
-  fds[0].events = POLLIN;
-
-  int    len, rc, on = 1;
-  int    new_sd = -1;
-  int    desc_ready, end_server = FALSE, compress_array = FALSE;
-  int    close_conn;
-  char   buffer[80];
-  int  current_size = 0, i, j;
-
-
-
-  /*************************************************************/
-  /* Loop waiting for incoming connects or for incoming data   */
-  /* on any of the connected sockets.                          */
-  /*************************************************************/
-  do {
-    printf("Waiting on poll()...\n");
-    if (poll(fds, nfds, -1) <= 0) {
-      error("poll() failed");
-      break;
-    }
-
-    current_size = nfds;
-    for (i = 0; i < current_size; i++)
-    {
-      if(fds[i].revents == 0)
-        continue;
-
-      if (fds[i].fd == listen_sd)
-      {
-        printf("  Listening socket is readable\n");
-
-        do {
-          new_sd = accept(listen_sd, NULL, NULL);
-          if (new_sd < 0)
-          {
-            if (errno != EWOULDBLOCK)
-            {
-              perror("  accept() failed");
-              end_server = TRUE;
-            }
-            break;
-          }
-
-          /*****************************************************/
-          /* Add the new incoming connection to the            */
-          /* pollfd structure                                  */
-          /*****************************************************/
-          printf("  New incoming connection - %d\n", new_sd);
-          fds[nfds].fd = new_sd;
-          fds[nfds].events = POLLIN;
-          nfds++;
-
-          /*****************************************************/
-          /* Loop back up and accept another incoming          */
-          /* connection                                        */
-          /*****************************************************/
-        } while (new_sd != -1);
-      }
-
-      /*********************************************************/
-      /* This is not the listening socket, therefore an        */
-      /* existing connection must be readable                  */
-      /*********************************************************/
-
-      else
-      {
-        printf("  Descriptor %d is readable\n", fds[i].fd);
-        close_conn = FALSE;
-        /*******************************************************/
-        /* Receive all incoming data on this socket            */
-        /* before we loop back and call poll again.            */
-        /*******************************************************/
-
-        do
-        {
-          /*****************************************************/
-          /* Receive data on this connection until the         */
-          /* recv fails with EWOULDBLOCK. If any other        */
-          /* failure occurs, we will close the                 */
-          /* connection.                                       */
-          /*****************************************************/
-          rc = recv(fds[i].fd, buffer, sizeof(buffer), 0);
-          if (rc < 0)
-          {
-            if (errno != EWOULDBLOCK)
-            {
-              perror("  recv() failed");
-              close_conn = TRUE;
-            }
-            break;
-          }
-
-          /*****************************************************/
-          /* Check to see if the connection has been           */
-          /* closed by the client                              */
-          /*****************************************************/
-          if (rc == 0)
-          {
-            printf("  Connection closed\n");
-            close_conn = TRUE;
-            break;
-          }
-
-          /*****************************************************/
-          /* Data was received                                 */
-          /*****************************************************/
-          len = rc;
-          printf("  %d bytes received\n", len);
-
-          /*****************************************************/
-          /* Echo the data back to the client                  */
-          /*****************************************************/
-          rc = send(fds[i].fd, buffer, len, 0);
-          if (rc < 0)
-          {
-            perror("  send() failed");
-            close_conn = TRUE;
-            break;
-          }
-
-        } while(TRUE);
-
-        /*******************************************************/
-        /* If the close_conn flag was turned on, we need       */
-        /* to clean up this active connection. This           */
-        /* clean up process includes removing the              */
-        /* descriptor.                                         */
-        /*******************************************************/
-        if (close_conn)
-        {
-          close(fds[i].fd);
-          fds[i].fd = -1;
-          compress_array = TRUE;
-        }
-
-
-      }  /* End of existing connection is readable             */
-    } /* End of loop through pollable descriptors              */
-
-    /***********************************************************/
-    /* If the compress_array flag was turned on, we need       */
-    /* to squeeze together the array and decrement the number  */
-    /* of file descriptors. We do not need to move back the    */
-    /* events and revents fields because the events will always*/
-    /* be POLLIN in this case, and revents is output.          */
-    /***********************************************************/
-    if (compress_array)
-    {
-      compress_array = FALSE;
-      for (i = 0; i < nfds; i++)
-      {
-        if (fds[i].fd == -1)
-        {
-          for(j = i; j < nfds; j++)
-          {
-            fds[j].fd = fds[j+1].fd;
-          }
-          nfds--;
-        }
-      }
-    }
-
-  } while (end_server == FALSE); /* End of serving running.    */
-
-  /*************************************************************/
-  /* Clean up all of the sockets that are open                  */
-  /*************************************************************/
-  for (i = 0; i < nfds; i++)
-  {
-    if(fds[i].fd >= 0)
-      close(fds[i].fd);
-  }
+        return 0;
 }
 
+/**
+ * This function will be called by libevent when the client socket is
+ * ready for reading.
+ */
+void
+on_read(int fd, short ev, void *arg)
+{
+	struct client *client = (struct client *)arg;
+	struct bufferq *bufferq;
+	u_char *buf;
+	int len;
+	
+	/* Because we are event based and need to be told when we can
+	 * write, we have to malloc the read buffer and put it on the
+	 * clients write queue. */
+	buf = malloc(BUFLEN);
+	if (buf == NULL)
+		err(1, "malloc failed");
 
+	len = read(fd, buf, BUFLEN);
+	if (len == 0) {
+		/* Client disconnected, remove the read event and the
+		 * free the client structure. */
+		printf("Client disconnected.\n");
+                close(fd);
+		event_del(&client->ev_read);
+		free(client);
+		return;
+	}
+	else if (len < 0) {
+		/* Some other error occurred, close the socket, remove
+		 * the event and free the client structure. */
+		printf("Socket failure, disconnecting client: %s",
+		    strerror(errno));
+		close(fd);
+		event_del(&client->ev_read);
+		free(client);
+		return;
+	}
 
+	/* We can't just write the buffer back as we need to be told
+	 * when we can write by libevent.  Put the buffer on the
+	 * client's write queue and schedule a write event. */
+	bufferq = calloc(1, sizeof(*bufferq));
+	if (bufferq == NULL)
+		err(1, "malloc faild");
+	bufferq->buf = buf;
+	bufferq->len = len;
+	bufferq->offset = 0;
+	TAILQ_INSERT_TAIL(&client->writeq, bufferq, entries);
 
+	/* Since we now have data that needs to be written back to the
+	 * client, add a write event. */
+	event_add(&client->ev_write, NULL);
+}
+
+/**
+ * This function will be called by libevent when the client socket is
+ * ready for writing.
+ */
+void
+on_write(int fd, short ev, void *arg)
+{
+	struct client *client = (struct client *)arg;
+	struct bufferq *bufferq;
+	int len;
+
+	/* Pull the first item off of the write queue. We probably
+	 * should never see an empty write queue, but make sure the
+	 * item returned is not NULL. */
+	bufferq = TAILQ_FIRST(&client->writeq);
+	if (bufferq == NULL)
+		return;
+
+	/* Write the buffer.  A portion of the buffer may have been
+	 * written in a previous write, so only write the remaining
+	 * bytes. */
+        len = bufferq->len - bufferq->offset;
+	len = write(fd, bufferq->buf + bufferq->offset,
+                    bufferq->len - bufferq->offset);
+	if (len == -1) {
+		if (errno == EINTR || errno == EAGAIN) {
+			/* The write was interrupted by a signal or we
+			 * were not able to write any data to it,
+			 * reschedule and return. */
+			event_add(&client->ev_write, NULL);
+			return;
+		}
+		else {
+			/* Some other socket error occurred, exit. */
+			err(1, "write");
+		}
+	}
+	else if ((bufferq->offset + len) < bufferq->len) {
+		/* Not all the data was written, update the offset and
+		 * reschedule the write event. */
+		bufferq->offset += len;
+		event_add(&client->ev_write, NULL);
+		return;
+	}
+
+	/* The data was completely written, remove the buffer from the
+	 * write queue. */
+	TAILQ_REMOVE(&client->writeq, bufferq, entries);
+	free(bufferq->buf);
+	free(bufferq);
+}
+
+/**
+ * This function will be called by libevent when there is a connection
+ * ready to be accepted.
+ */
+void
+on_accept(int fd, short ev, void *arg)
+{
+	int client_fd;
+	struct sockaddr_in client_addr;
+	socklen_t client_len = sizeof(client_addr);
+	struct client *client;
+
+	/* Accept the new connection. */
+	client_fd = accept(fd, (struct sockaddr *)&client_addr, &client_len);
+	if (client_fd == -1) {
+		warn("accept failed");
+		return;
+	}
+
+	/* Set the client socket to non-blocking mode. */
+	if (setnonblock(client_fd) < 0)
+		warn("failed to set client socket non-blocking");
+
+	/* We've accepted a new client, allocate a client object to
+	 * maintain the state of this client. */
+	client = calloc(1, sizeof(*client));
+	if (client == NULL)
+		err(1, "malloc failed");
+
+	/* Setup the read event, libevent will call on_read() whenever
+	 * the clients socket becomes read ready.  We also make the
+	 * read event persistent so we don't have to re-add after each
+	 * read. */
+	event_set(&client->ev_read, client_fd, EV_READ|EV_PERSIST, on_read, 
+	    client);
+
+	/* Setting up the event does not activate, add the event so it
+	 * becomes active. */
+	event_add(&client->ev_read, NULL);
+
+	/* Create the write event, but don't add it until we have
+	 * something to write. */
+	event_set(&client->ev_write, client_fd, EV_WRITE, on_write, client);
+
+	/* Initialize the clients write queue. */
+	TAILQ_INIT(&client->writeq);
+
+	printf("Accepted connection from %s\n",
+               inet_ntoa(client_addr.sin_addr));
+}
+
+int
+main(int argc, char **argv)
+{
+	int listen_fd;
+	struct sockaddr_in listen_addr;
+	int reuseaddr_on = 1;
+
+	/* The socket accept event. */
+	struct event ev_accept;
+
+	/* Initialize libevent. */
+	event_init();
+
+	/* Create our listening socket. This is largely boiler plate
+	 * code that I'll abstract away in the future. */
+	listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (listen_fd < 0)
+		err(1, "listen failed");
+	if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &reuseaddr_on, 
+		sizeof(reuseaddr_on)) == -1)
+		err(1, "setsockopt failed");
+	memset(&listen_addr, 0, sizeof(listen_addr));
+	listen_addr.sin_family = AF_INET;
+	listen_addr.sin_addr.s_addr = INADDR_ANY;
+	listen_addr.sin_port = htons(SERVER_PORT);
+	if (bind(listen_fd, (struct sockaddr *)&listen_addr,
+		sizeof(listen_addr)) < 0)
+		err(1, "bind failed");
+	if (listen(listen_fd, 5) < 0)
+		err(1, "listen failed");
+
+	/* Set the socket to non-blocking, this is essential in event
+	 * based programming with libevent. */
+	if (setnonblock(listen_fd) < 0)
+		err(1, "failed to set server socket to non-blocking");
+
+	/* We now have a listening socket, we create a read event to
+	 * be notified when a client connects. */
+	event_set(&ev_accept, listen_fd, EV_READ|EV_PERSIST, on_accept, NULL);
+	event_add(&ev_accept, NULL);
+
+	/* Start the libevent event loop. */
+	event_dispatch();
+
+	return 0;
+}
