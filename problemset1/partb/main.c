c4944c3ce51924ab647604f2b0d27a5ab6187dd8
Konstantin Weitz <weitzkon@cs.uw.edu>
2016-10-05 14:02:51 -0700

    add main

---
+++ problemset1/partb/main.c
@@ -0,0 +1,18 @@
+
+; thread pool (pthreads)
+
+; worker takes file name, read file, returns a pointer to the file
+
+main 
+
+; args : ip, port
+
+
+
+; open tcp connection
+; read file path, load file, return over tcp
+; close tcp connection
+
+
+
+
